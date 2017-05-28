3b37972cd18a7011329cbdd898e74244121c0012
Chad Hardin <cehardin@outlook.com>
2017-05-28 13:13:53 -0700

    Further simplied vertex selectiojn

--- src/main/java/edu/nova/chardin/patrol/agent/strategy/covering/AbstractScoringCoveringStrategy.java
+++ src/main/java/edu/nova/chardin/patrol/agent/strategy/covering/AbstractScoringCoveringStrategy.java
@@ -1,18 +1,15 @@
 package edu.nova.chardin.patrol.agent.strategy.covering;
 
-import com.google.common.collect.BoundType;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Range;
 import edu.nova.chardin.patrol.agent.AgentContext;
 import edu.nova.chardin.patrol.graph.VertexId;
 import lombok.NonNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.NoSuchElementException;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public abstract class AbstractScoringCoveringStrategy implements CoveringStrategy {
 
@@ -50,8 +47,84 @@ public abstract class AbstractScoringCoveringStrategy implements CoveringStrateg
 
   private VertexId selectVertexFromScores(@NonNull final Map<VertexId, Double> vertexScores) {
 
+    final Double max = vertexScores.values().stream().max(Double::compare).get();
+    final List<VertexId> vertices = vertexScores.entrySet().stream()
+            .filter(e -> e.getValue().equals(max))
             .map(Entry::getKey)
+            .collect(Collectors.toList());
+    final int vertexCount = vertices.size();
+    
+    switch (vertexCount) {
+      case 0:
+        throw new IllegalStateException();
+      case 1:
+        return vertices.get(0);
+      default:
+        return vertices.get(ThreadLocalRandom.current().nextInt(vertexCount));
     }
+    
+//    final Map<VertexId, Range<Double>> wheel = new HashMap<>(vertexScores.size());
+//    final double pick;
+//
+//    {
+//      final double minScore = vertexScores.values().stream().mapToDouble(Double::doubleValue).min().getAsDouble();
+//
+//      if (minScore < 0) {
+//        vertexScores.replaceAll((v, s) -> s + Math.abs(minScore));
+//      }
+//    }
+//
+//    if (vertexScores.values().stream().anyMatch(s -> s < 0)) {
+//      throw new AssertionError(String.format("Negative scores found : %s", vertexScores));
+//    }
+//
+//    {
+//      final double scoresSum = vertexScores.values().stream().mapToDouble(Double::doubleValue).sum();
+//      double max = 0.0;
+//
+//      for (final Entry<VertexId, Double> vertexScore : vertexScores.entrySet()) {
+//        final VertexId vertexId = vertexScore.getKey();
+//        final double rawScore = vertexScore.getValue();
+//        final double weigtedScore = scoresSum == 0.0 ? 0.0 : rawScore / scoresSum;
+//        final double offset = max;
+//        final Range<Double> range;
+//        max += weigtedScore;
+//        
+//        range = offset == max ? Range.singleton(offset) : Range.closedOpen(offset, max);
+//        
+//        wheel.put(vertexId, range);
+//      }
+//      
+//      if (max == 0.0) {
+//        pick = 0.0;
+//      } else {
+//        try {
+//          pick = ThreadLocalRandom.current().nextDouble(max);
+//        } catch (IllegalArgumentException e) {
+//          throw new IllegalStateException(
+//                  String.format(
+//                          "Could not get next double with a max of %f from : %s", 
+//                          max, 
+//                          vertexScores), 
+//                  e);
+//        }
+//      }
+//    }
+//
+//    try {
+//      return wheel.entrySet().stream()
+//            .filter(e -> e.getValue().contains(pick))
+//            .map(Entry::getKey)
+//            .findFirst()
+//            .get();
+//    } catch (NoSuchElementException e) {
+//      throw new IllegalStateException(
+//              String.format(
+//                      "Could not find %f in %s", 
+//                      pick, 
+//                      wheel), 
+//              e);
+//    }
   }
 
   protected abstract double scoreArrival(int attackInterval, int arrivalTimestep, int returnTimestep);
